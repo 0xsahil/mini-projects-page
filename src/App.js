e49ccc479ce5d1b0fc894899654715a548c60a1d
0xsahil <85027015+0xsahil@users.noreply.github.com>
2023-10-19 18:01:30 +0530

    new project added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Projects from './components/Projects';
 
 function App() {
   return (
-    <>
+    <div className='relative w-full max-w-5xl bg-black mx-auto'>
       <div className='relative max-w-3xl mx-auto flex items-center flex-col ' >
           <Nav />
           <Projects />
         </div>
-    </>
+    </div>
   );
 }
 
--- src/assets/assets.jsx
+++ src/assets/assets.jsx
@@ -1,16 +1,24 @@
 import cashApp from "./cashAppLogo.svg";
-import form from "./form.png";
+import form from "./from.webp";
+import event from "./event.png";
 
 const data = [
   {
     id: 1,
+    title: "EVENT",
+    description: "Admin platform to organize events",
+    link: "https://event-0xsahil.netlify.app/",
+    image: event,
+  },
+  {
+    id: 2,
     title: "CASH APP",
     description: "Landing page design for mobile app",
     link: "https://cash-app-0xsahil.netlify.app/",
     image: cashApp,
   },
   {
-    id: 2,
+    id: 3,
     title: "SIGN UP",
     description: "Custom form for signing up using google forms API",
     link: "https://0xsahil.github.io/customForms/",
@@ -18,4 +26,4 @@ const data = [
   },
 ];
 
-export { data };
+export { data};
