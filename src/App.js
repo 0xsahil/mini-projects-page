df6cbe527873438b67b31919e8894d84de1b8344
SB Verma <0xsahilv@gmail.com>
2024-04-27 15:54:51 +0530

    Fetched data from BE , added new footer, improved layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
-import Nav from './components/Nav';
-import Projects from './components/Projects';
+import "./App.css";
+import Nav from "./components/Nav";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className='relative w-full max-w-5xl bg-black mx-auto'>
-      <div className='relative max-w-3xl mx-auto flex items-center flex-col ' >
-          <Nav />
-          <Projects />
-        </div>
+    <div className="relative w-full h-full bg-black bg-opacity-90 mx-auto">
+      <div className="relative mx-auto flex items-center flex-col ">
+        <Nav />
+        <Projects />
+        <Footer />
+      </div>
     </div>
   );
 }
